45a3eaff453407510731ce1359eb51d753e6319f
m-ehab2 <muhamedehab1@gmail.com>
2024-01-26 17:15:06 +0200

    Finished Some sections of the sass assignment

--- Sass/index.js
+++ Sass/index.js
@@ -13,5 +13,22 @@ for (let index = 0; index < li.length; index++) {
         e.target.classList.toggle('active');
     });
 }
+const medicalRow = document.getElementById("medicalRow");
+
+const observer = new IntersectionObserver(
+    (entries, observer) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("active");
+                observer.unobserve(entry.target);
+            }
+        });
+    },
+    { threshold: 0.5 }
+);
+
+observer.observe(medicalRow);
+
+
 
 console.log(li);
--- Sass/script.js
+++ Sass/script.js
@@ -7,4 +7,13 @@
 //     loop: false,
 //     controlsContainer: '.carousel__controls'
 // });
+var slider = tns({
+    container: '.slider--hero',
+    mode: "gallery",
+    items: 1,
+    slideBy: 1,
+    controls: false,
+    speed: 700,
+    navContainer: '.slider__nav'
+});
 
